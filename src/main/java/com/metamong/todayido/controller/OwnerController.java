7193a715b5350742c1250ec6687f9359af2a26bc
KimTae Uk <152362794+Taeuking2529@users.noreply.github.com>
2024-01-02 16:57:00 +0900

    가게등록/css/서비스 추가

--- src/main/java/com/metamong/todayido/controller/OwnerController.java
+++ src/main/java/com/metamong/todayido/controller/OwnerController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -17,35 +19,60 @@ public class OwnerController {
     private OwnerService oServ;
 
     @GetMapping("ownerJoin")
-    public String ownerJoin(){
+    public String ownerJoin() {
         log.info("ownerJoin()");
         return "ownerJoin";
     }
+
     @PostMapping("ownerjoinProc")
     public String ownerjoinProc(OwnerDto owner, RedirectAttributes rttr) {
         log.info("ownerjoinProc()");
         String view = oServ.ownerJoin(owner, rttr);
         return view;
     }
+
     @GetMapping("ownerLogin")
-    public String ownerLogin(){
+    public String ownerLogin() {
         log.info("ownerLogin()");
         return "ownerLogin";
     }
+
     @PostMapping("ownerloginProc")
-    public String ownerloginProc(OwnerDto owner, HttpSession session, RedirectAttributes rttr){
+    public String ownerloginProc(OwnerDto owner, HttpSession session, RedirectAttributes rttr) {
         log.info("ownerloginProc()");
         String view = oServ.ownerloginProc(owner, session, rttr);
         return view;
     }
+
     @GetMapping("pindex")
-    public String pindex(){
+    public String pindex() {
         log.info("pindex");
         return "pindex";
     }
+
     @GetMapping("ownerPage")
-    public String ownerPage(){
+    public String ownerPage() {
         log.info("ownerPage()");
         return "ownerPage";
     }
+
+    //가게등록하기
+    @GetMapping("pdetail")
+    public String pdetail() {
+        log.info("pdetail()");
+        return "pdetail";
+    }
+
+    @PostMapping("/pdetail")
+    public String pdetail(@RequestParam("file") MultipartFile file,
+                          OwnerDto pdetail, HttpSession session,
+                          RedirectAttributes rttr) {
+        log.info("pdetail()");
+        String uploadResult = oServ.pdetail(file, pdetail, session, rttr);
+        if ("File uploaded successfully.".equals(uploadResult)) {
+        } else {
+        }
+        return "redirect:/";
+    }
 }
+
