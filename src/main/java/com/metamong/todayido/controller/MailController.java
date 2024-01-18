969370faa4563e1f1870a6d39426398288178249
hyoung29 <152365351+hyoung29@users.noreply.github.com>
2024-01-18 10:48:23 +0900

    다듬기

--- src/main/java/com/metamong/todayido/controller/MailController.java
+++ src/main/java/com/metamong/todayido/controller/MailController.java
@@ -1,7 +1,6 @@
 package com.metamong.todayido.controller;
 
 import com.metamong.todayido.service.MailService;
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @Slf4j
